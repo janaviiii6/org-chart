ec525c5ae75e886ffeea4cc6afb3205061c11e50
JanaviK <kharatjanavi16@gmail.com>
2024-12-11 13:37:49 +0530

    Add database connection file

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+//Database connection details
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'org_chart';
+
+
+try{
+    //Create new PDO instance
+    $pdo = new PDO("mysql:host=$host;db=$db",$username,$password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+?>
